78affcd15411da9ae4c479119013b21ef30a83c4
Akshay Ashok <aks28id@gmail.com>
2020-12-10 15:16:12 +0530

    fix: tailwind prod issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'media', // 'media' or 'class'
   theme: {
     colors: {
